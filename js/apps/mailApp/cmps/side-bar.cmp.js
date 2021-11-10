813d066e1078aa89e4f1392aa11f04ec82511720
koren levi <koren012349@gmail.com>
2021-11-10 15:36:59 +0200

    koren

---
+++ js/apps/mailApp/cmps/side-bar.cmp.js
@@ -0,0 +1,12 @@
+
+
+
+export default {
+    template: `
+    <div class="email-side-bar ">
+        <button class="mail-compose-btn flex justify-center align-center">
+        <img :key="'img'" src="assets/img/plus.png" class="inline">
+        </button>
+    </div>
+    `
+}
