bdb2fb4d1fa23203b51c762c006d3e6364f33870
koren levi <koren012349@gmail.com>
2021-11-10 14:57:26 +0200

    8

--- js/apps/mailApp/pages/email-app.cmp.js
+++ js/apps/mailApp/pages/email-app.cmp.js
@@ -2,15 +2,7 @@ console.log('hi');
 // require('@/css/email.css');
 export default {
     template: `
-
-    <!-- <style scoped lang="css">
-    @import './js/mail';
-    </style> -->
-    
-    
-    
     <main class="email-app">
-    <!-- <link rel="stylesheet" href="css/email.css"> -->
         <h1>email</h1>
     </main>
     `,
