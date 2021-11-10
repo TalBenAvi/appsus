7a9fb7d2c9b3be03732475bfee2bf54776e2a657
tal ben-avi <tal@ben-avi.com>
2021-11-10 11:54:46 +0200

    routes

--- js/routes.js
+++ js/routes.js
@@ -39,18 +39,18 @@ const routes = [
             },
         ]
     },
-    {
-        path: '/book',
-        component: bookApp
-    },
-    {
-        path: '/book/:bookId?/edit',
-        component: bookEdit
-    },
-    {
-        path: '/book/:bookId',
-        component: bookDetails
-    },
+    // {
+    //     path: '/book',
+    //     component: bookApp
+    // },
+    // {
+    //     path: '/book/:bookId?/edit',
+    //     component: bookEdit
+    // },
+    // {
+    //     path: '/book/:bookId',
+    //     component: bookDetails
+    // },
 ];
 
 export const router = new VueRouter({ routes });
