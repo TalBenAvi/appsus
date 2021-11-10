2f7905842094d2387e733c7caad794e213bf2776
tal ben-avi <tal@ben-avi.com>
2021-11-10 12:03:13 +0200

    css

--- js/main.js
+++ js/main.js
@@ -3,10 +3,11 @@ import appFooter from './cmps/app-footer.cmp.js';
 import { router } from './routes.js';
 const options = {
     el: '#app',
+    router,
     template: `
     <app-header></app-header>
-    <router-view></router-view>
-    <app-footer>   </app-footer>
+    <!-- <router-view></router-view> -->
+    <app-footer></app-footer>
 
     `,  components: {
         appHeader,
