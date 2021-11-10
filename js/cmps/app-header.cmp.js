c3db00db79ef857a44a47578d40e22285982684b
tal ben-avi <tal@ben-avi.com>
2021-11-10 11:22:29 +0200

    sdsd

---
+++ js/cmps/app-header.cmp.js
@@ -0,0 +1,13 @@
+export default {
+    template: `
+        <header class="app-header">
+            <h1>Books</h1>
+            </div>
+            <nav>
+                <router-link to="/" active-class="active-link" exact>Home</router-link> |
+                <router-link to="/book" active-class="active-link" >Books</router-link> |
+                <router-link to="/about" active-class="active-link" >About</router-link>
+            </nav>
+        </header>
+    `,
+}
