9c711fcdd6fa217753078e74e622f05ba704a97d
tal ben-avi <tal@ben-avi.com>
2021-11-10 12:34:41 +0200

    rotes

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -7,7 +7,8 @@ export default {
                 <router-link to="/" active-class="active-link" exact>Home</router-link> |
                 <router-link to="/books" active-class="active-link" exact>Books</router-link> |
                 <router-link to="/email" active-class="active-link" >Email</router-link> |
-                <router-link to="/notes" active-class="active-link" >Notes</router-link>
+                <router-link to="/notes" active-class="active-link" >Notes</router-link> |
+                <router-link to="/about" active-class="active-link" >About</router-link>
             </nav>
         </header>
     `,
