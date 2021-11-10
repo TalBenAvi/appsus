c811c98c5d233d68444f9488db4b7cac6e2d9060
koren levi <koren012349@gmail.com>
2021-11-10 16:57:05 +0200

    10

--- js/apps/mailApp/cmps/side-bar.cmp.js
+++ js/apps/mailApp/cmps/side-bar.cmp.js
@@ -2,11 +2,47 @@
 
 
 export default {
+   
     template: `
-    <div class="email-side-bar ">
+    <aside class="email-side-bar">
         <button class="mail-compose-btn flex justify-center align-center">
         <img :key="'img'" src="assets/img/plus.png" class="inline">
         </button>
-    </div>
-    `
+
+        <section class="email-menu flex">
+            <a :class="{active: isActive}" @click="makeActive">All</a>
+            <a :class="{active: isActive}" @click="isActive = !isActive">Inbox</a>
+            <a :class="{active: isActive}" @click="isActive = !isActive">Sent</a>
+            <a :class="{active: isActive}" @click="isActive = !isActive">Archive</a>
+            <a :class="{active: isActive}" @click="isActive = !isActive">Drafts</a>
+            <a :class="{active: isActive}" @click="isActive = !isActive">Trash</a>
+        </section>
+
+
+    </aside>
+    `,
+    data(){
+        return {
+            
+                isActive: false,
+            
+            
+        }
+    },
+    methods: {
+        makeActive(event){
+            // console.log(event);
+            this.isActive = !this.isActive;
+        }
+
+    }
 }
+
+{/* <section class="email-menu flex">
+<a :class="{active: isActive}" @click="isActive = !isActive">All</a>
+<a class="" v-class="{active: isActive}" @click="makeActive">Inbox</a>
+<a class="" v-class="{active: isActive}" @click="makeActive">Sent</a>
+<a class="" v-class="{active: isActive}" @click="makeActive">Archive</a>
+<a class="" v-class="{active: isActive}" @click="makeActive">Drafts</a>
+<a class="" v-class="{active: isActive}" @click="makeActive">Trash</a>
+</section> */}
--- js/apps/mailApp/pages/email-app.cmp.js
+++ js/apps/mailApp/pages/email-app.cmp.js
@@ -3,8 +3,8 @@ import sideBar from '../cmps/side-bar.cmp.js';
 
 export default {
     template: `
-    <main class="email-app">
-        <h1>email</h1>
+    <main class="email-app flex">
+        <!-- <h1>email</h1> -->
         <side-bar></side-bar>
     </main>
     `,
